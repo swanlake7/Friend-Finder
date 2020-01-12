6244aea9f198fcf98a2884eb7604c552204b0185
Pearl Jean <perlegirl@protonmail.com>
2020-01-12 14:39:37 -0500

    initial setup

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+let fList = [
+    {
+        "name": "Dark Knight",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.giantbomb.com%2Fuploads%2Fscale_medium%2F8%2F84290%2F2794847-0704837456-icon10.jpeg&f=1&nofb=1",
+        "scores": [
+            1,
+            3,
+            1,
+            3,
+            2,
+            1,
+            6,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Harley Quin",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcelebsoutfits.com%2Fwp-content%2Fuploads%2F2019%2F04%2FMargot-Robbie-Harley-Quinn-Jacket-in-Suicide-Squad-Movie.jpg&f=1&nofb=1",
+        "scores": [
+            2,
+            2,
+            3,
+            3,
+            2,
+            4,
+            2,
+            2,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "Naruto Uzumaki",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F6Upmn2Zf7h4%2Fmaxresdefault.jpg&f=1&nofb=1",
+        "scores": [
+            3,
+            5,
+            4,
+            5,
+            3,
+            5,
+            3,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Winnie the",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages7.alphacoders.com%2F521%2F521449.jpg&f=1&nofb=1",
+        "scores": [
+            4,
+            1,
+            1,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Abel Tesfaye",
+        "photo": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F28%2Ff4%2F50%2F28f45087598a71276af69def4927e416.jpg&f=1&nofb=1",
+        "scores": [
+            5,
+            5,
+            1,
+            1,
+            5,
+            1,
+            5,
+            4,
+            3,
+            5
+        ]
+    }
+];
+
+module.exports = fList;
